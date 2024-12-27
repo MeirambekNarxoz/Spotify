6ac049910e46419d57b07e281020e9f81764eb80
MeirambekNarxoz <meirambek.alikulov@narxoz.kz>
2024-12-27 19:07:04 +0500

    Login Register

--- src/main/java/spotify/Config/SecurityConfiguration.java
+++ src/main/java/spotify/Config/SecurityConfiguration.java
@@ -17,20 +17,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/songs/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/songs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/person/mainPage").permitAll()
-                        .requestMatchers("/songs/").permitAll()
+                        .requestMatchers("/songs/**").hasRole("ADMIN")
                         .requestMatchers("/person/login", "/person/register").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/person/").permitAll()
-                        .requestMatchers("/person/").permitAll()
-                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/person/**").permitAll()
+                        .requestMatchers("/person/**").permitAll()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
